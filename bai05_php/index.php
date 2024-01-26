91636d17d34119d447196604c351a8fc76739e6e
tnhuyhoang1605 <157784538+tnhuyhoang1605@users.noreply.github.com>
2024-01-26 13:58:39 +0700

    Add files via upload

---
+++ bai05_php/index.php
@@ -0,0 +1,25 @@
+<?php
+$t = date('H');
+if($t <'10'){
+    echo 'Have a good morning';
+}elseif($t < '20'){
+    echo 'Have a good day';
+}else{
+    echo 'Have a good night';
+}
+echo '<br>';
+var_dump($t);
+//Giai phuong trinh bac nhat ax + b = 0;
+$a = 5;
+$b = 6;
+if($a == 0){
+    if($b ==0){
+        echo 'Phuong trinh co vo so nghiem';
+    }else{
+        echo 'Phuong trinh vo nghiem';
+    }
+}else{
+    $x = -$b / $a;
+    echo "Pt co nghiem: $x";
+}   
+?>
